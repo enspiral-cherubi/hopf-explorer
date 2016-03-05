1495ca89067918d2c506dc3ed75ba3db0fb4ae18
euglazer <eugenestapler@gmail.com>
2016-03-05 15:57:24 -0800

    clear background

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var THREE = require('three')
+var OrbitControls = require('three-orbit-controls')(THREE)
 
 var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
 
-var renderer = new THREE.WebGLRenderer()
+var renderer = new THREE.WebGLRenderer({alpha: true})
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setClearColor(0xffffff, 0)
 document.body.appendChild(renderer.domElement)
 
 var geometry = new THREE.BoxGeometry(1, 1, 1)
