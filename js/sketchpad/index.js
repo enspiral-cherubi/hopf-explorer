74b9c87edcfa21ca5778710e1fd608b4bc5feed1
euglazer <eugenestapler@gmail.com>
2016-03-05 22:27:06 -0800

    capturing spherical coords on sketchpad click

--- js/sketchpad/index.js
+++ js/sketchpad/index.js
@@ -1,16 +1,24 @@
 var $ = require('jquery')
-var range = require('lodash.range')
+var mag = require('vectors/mag')(2)
 
 var sketchpad = {
   $sketchpad: $('#sketchpad'),
   context: $('#sketchpad')[0].getContext('2d'),
   paint: false,
-  clickX: [],
-  clickY: [],
+  sphericalCoordsArray: [],
 
   init: function () {
     this.context.fillStyle = '#FFFFFF'
     this.bindEventListeners()
+  },
+
+  addSphericalCoords: function (x,y) {
+    x = x - this.$sketchpad.width() / 2
+    y = y - this.$sketchpad.height() / 2
+    this.sphericalCoordsArray.push({
+      eta: Math.atan(1/mag([x,y])),
+      phi: Math.atan(y/x)
+    })
   },
 
   bindEventListeners: function () {
@@ -18,16 +26,19 @@ var sketchpad = {
 
     this.$sketchpad.mousedown(function (e) {
       self.paint = true
+      self.addSphericalCoords(e.offsetX, e.offsetY)
       self.drawPoint(e.offsetX, e.offsetY)
     })
 
-    this.$sketchpad.mouseup(function (e) { self.paint = false })
-
-    this.$sketchpad.mouseleave(function(e){ self.paint = false })
-
     this.$sketchpad.mousemove(function (e) {
-      if (self.paint) { self.drawPoint(e.offsetX, e.offsetY) }
+      if (self.paint) {
+        self.addSphericalCoords(e.offsetX, e.offsetY)
+        self.drawPoint(e.offsetX, e.offsetY)
+      }
     })
+
+    this.$sketchpad.mouseup(function (e) { self.paint = false })
+    this.$sketchpad.mouseleave(function(e){ self.paint = false })
   },
 
   drawPoint: function (x,y) {
