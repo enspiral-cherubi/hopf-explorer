f8985f00a3c184c7fd2ea25fffbdbcf7006ae9d4
ragnar <ryan.thorngren@berkeley.edu>
2016-03-13 05:10:05 -0700

    crazy particle FLOWS. math is still wonky but cool af

--- js/environment/flow.js
+++ js/environment/flow.js
@@ -1,18 +1,27 @@
 var THREE = require('three')
 var mag = require('vectors/mag')(3)
 
-var getFlow = function (x,y,z,dt) {
+var getFlow = function (vect,dt) {
   //the variable names correspond to those in generate-fiber.js
-  var r = Math.sqrt(x*x+y*y+z*z)
-  var x4 = (1-r)/(1+r)
+  var r = Math.sqrt(vect.x*vect.x+vect.y*vect.y+vect.z*vect.z)
+  var x4 = (1-r*r)/(1+r*r)
   var m = Math.sqrt(1-x4*x4)
-  var drdt = -Math.pow(1+x4,-2)/r
-  var dminvdt = 1/(r*(1-x4*x4))*(-x+y+r*x4)
-  return new THREE.Vector3(
-    dt*((x/r)*drdt + (x*m)*dminvdt + y),
-    dt*((y/r)*drdt + (y*m)*dminvdt - x),
-    dt*((z/r)*drdt + (z*m)*dminvdt + r*x4/m)
-  )
+  var x2 = m*vect.x/r
+
+  var x1 = m*vect.y/r
+
+  var x3 = m*vect.z/r
+
+  var dmdt = (1/m)*x3*x4
+
+  var drdt = x3/(r*Math.pow(1+x4,2))
+
+  var unscaledFlow = new THREE.Vector3(
+    r*x2*drdt+r*x1/m-r*x2*dmdt/Math.pow(m,2),
+    r*x1*drdt-r*x2/m-r*x1*dmdt/Math.pow(m,2),
+    r*x3*drdt+r*x4/m-r*x3*dmdt/Math.pow(m,2)
+)
+  return unscaledFlow.multiplyScalar(dt)
 }
 
 module.exports = getFlow
--- js/environment/index.js
+++ js/environment/index.js
@@ -43,9 +43,7 @@ module.exports = {
       var deltaMsec = Math.min(200, nowMsec - lastTimeMsec)
       lastTimeMsec  = nowMsec
       //particles traverse the circle every 2pi seconds
-      self.particles.forEach(function (particle){
-        self.move(deltaMsec/1000)
-      })
+      self.move(deltaMsec/2000)
 
       if (self.controls) { self.controls.update(deltaMsec/1000) }
     })
@@ -54,8 +52,7 @@ module.exports = {
   move: function(dt) {
     this.particles.forEach(function (particle){
       var pos = particle.position
-      particle.position.add(getFlow(pos.x,pos.y,pos.z,dt))
-      console.log(getFlow(pos.x,pos.y,pos.z,dt))
+      particle.position.add(getFlow(pos,dt))
     })
   },
 
