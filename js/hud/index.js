c3a175b4d985727b6694b0a380a13f0ad7a8e57d
euglazer <eugenestapler@gmail.com>
2016-03-14 00:17:10 -0700

    refactor hud/index

--- js/hud/index.js
+++ js/hud/index.js
@@ -5,20 +5,24 @@ var apod = require('nasa-apod')
 
 var hud = {
   sketchpad: sketchpad,
+
   init: function(env) {
     this.sketchpad.init(env)
-    setBackgroundImage()
+    this.setBackgroundImage()
+  },
+
+  setBackgroundImage: function () {
+    var date = randomDate(new Date(2012, 0, 1),new Date())
+    apod(date).then(function(data) {
+      $('body').css({
+        'background-image': 'url(' + data.hdurl + ')',
+        'background-position': 'center',
+        'background-size': 'cover',
+        'background-repeat': 'no-repeat'
+      })
+    })
   }
 }
 
-var setBackgroundImage = function () {
-  var date = randomDate(new Date(2012, 0, 1),new Date())
-  apod(date).then(function(data) {
-      $('body').css("background-image","url("+data.hdurl+")")
-      $('body').css("background-position","center")
-      $('body').css("background-size","cover")
-      $('body').css("background-repeat","no-repeat")
-    })
-}
 
 module.exports = hud
