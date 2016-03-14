0991b14eb5f9269212fab2901b07e70b7ca4bb8e
ragnar <ryan.thorngren@berkeley.edu>
2016-03-13 20:13:55 -0700

    gets astronomy picture of the day, blows it up to 150% and uses that as the background

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 var environment = require('./environment')
+var apod = require('nasa-apod')
+var $ = require('jquery')
+
+apod().then(function(data) {
+    $('#body').css("background-image","url("+data.url+")")
+    $('#body').css("background-position","center")
+    $('#body').css("background-size","150%")
+    $('#body').css("background-repeat","no-repeat")
+    console.log(data.url)
+})
+
 
 environment.init()
 environment.startAnimation()
