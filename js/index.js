c37e97fce77a710a04c4fa12a9fde6f816b1aa14
ragnar <ryan.thorngren@berkeley.edu>
2016-03-13 20:23:07 -0700

    background fits window

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var $ = require('jquery')
 apod().then(function(data) {
     $('#body').css("background-image","url("+data.url+")")
     $('#body').css("background-position","center")
-    $('#body').css("background-size","150%")
+    $('#body').css("background-size","cover")
     $('#body').css("background-repeat","no-repeat")
     console.log(data.url)
 })
