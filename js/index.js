96ed5e100cd308145b5cd2ca4c13c01140b67b75
ragnar <ryan.thorngren@berkeley.edu>
2016-03-13 21:02:24 -0700

    gets random astronomy picture of the day

--- js/index.js
+++ js/index.js
@@ -2,8 +2,14 @@ var environment = require('./environment')
 var apod = require('nasa-apod')
 var $ = require('jquery')
 
-apod().then(function(data) {
-    $('#body').css("background-image","url("+data.url+")")
+var randomDate = function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+var date = randomDate(new Date(2012, 0, 1), new Date())
+
+apod(date).then(function(data) {
+    $('#body').css("background-image","url("+data.hdurl+")")
     $('#body').css("background-position","center")
     $('#body').css("background-size","cover")
     $('#body').css("background-repeat","no-repeat")
