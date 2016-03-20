7e77136f38cec33bbcdcb5f25764c252281645c8
ragnar <ryan.thorngren@berkeley.edu>
2016-03-19 23:00:31 -0700

    generateParticle now takes an optional argument start that says where along the trajectory the particle begins its journey, modulo 2pi

--- js/environment/generate-particle.js
+++ js/environment/generate-particle.js
@@ -3,7 +3,8 @@ var mag = require('vectors/mag')(3)
 var hexStringFromSphericalCoords = require('./../services/hex-string-from-spherical-coords')
 
 // TODO: give to world
-var generateParticle = function (sphericalCoords) {
+var generateParticle = function (sphericalCoords,start) {
+  start = start || 0
   var particle = new THREE.DodecahedronGeometry(0.1)
 
   var material = new THREE.MeshNormalMaterial()
@@ -12,10 +13,10 @@ var generateParticle = function (sphericalCoords) {
   var eta = sphericalCoords.eta, phi = sphericalCoords.phi
 
   //get point on unit 3-sphere in 4D space with Cartesian coordinates
-  var x1 = Math.cos(phi) * Math.sin(eta/2)
-  var x2 = Math.sin(phi) * Math.sin(eta/2)
-  var x3 = Math.cos(phi) * Math.cos(eta/2)
-  var x4 = Math.sin(phi) * Math.cos(eta/2)
+  var x1 = Math.cos(phi+start) * Math.sin(eta/2)
+  var x2 = Math.sin(phi+start) * Math.sin(eta/2)
+  var x3 = Math.cos(phi+start) * Math.cos(eta/2)
+  var x4 = Math.sin(phi+start) * Math.cos(eta/2)
 
   //stereographically project point on 3-sphere to point in 3D space in Cartesian coordinates
   var m = mag([x1,x2,x3])
