9d17e0e5432fa070ad02cc4a7969c8435852c689
Spencer Hitchcock <spencerhitch@gmail.com>
2016-03-19 22:03:48 -0700

    Index js audio support.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
 var environment = require('./environment')
+var _ = require('./underscore')
+
+var $ = require('jquery')
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+
+var webAudioAnalyser2 = require('web-audio-analyser-2')
+var analyser = webAudioAnalyser2({
+      context: audioCtx,
+        fftSize: 2048,
+          equalTemperedFreqBinCount: 10
+})
+ 
+analyser.connect(audioCtx.destination)
+ 
+var micSelector = require('mic-selector')(audioCtx)
+ 
+var $micSelector = $(micSelector)
+ 
+$micSelector.on('bang', function (e, node) {
+      node.connect(analyser)
+})
+
+$('body').append($micSelector)
+var interval = setInterval(function () {
+var barkScaleFrequencyData = analyser.barkScaleFrequencyData()
+barkScaleFrequencyData = _.map(barkScaleFrequencyData, function (e) -
+
+console.log('barkScaleFrequencyData.frequencies: ', barkScaleFrequencyData.frequencies)
+//console.log('barkScaleFrequencyData.overallAmplitude: ', barkScaleFrequencyData.overallAmplitude)
+}, 50)
 
 environment.init()
 environment.startAnimation()
