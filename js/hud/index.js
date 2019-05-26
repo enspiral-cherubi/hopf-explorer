625af79612613e626146fe7b16e7a10e6746c64e
Ryan Thorngren <ryan.thorngren@gmail.com>
2019-05-26 17:33:17 +0300

    fixed the spiral

--- js/hud/index.js
+++ js/hud/index.js
@@ -1,21 +1,21 @@
 var sketchpad = require('./sketchpad')
 var $ = require('jquery')
 var apod = require('apod')
-apod.apiKey = process.env.NASA_API_KEY
+// apod.apiKey = process.env.NASA_API_KEY
 
 var hud = {
   sketchpad: sketchpad,
 
   init: function(env) {
     this.sketchpad.init(env)
-    this.setBackgroundImage()
+    // this.setBackgroundImage()
   },
 
-  setBackgroundImage: function () {
-    apod.random(function (err, res) {
-      $('body').css('background-image', 'url(' + res.hdurl + ')')
-    })
-  }
+  // setBackgroundImage: function () {
+  //   apod.random(function (err, res) {
+  //     $('body').css('background-image', 'url(' + res.hdurl + ')')
+  //   })
+  // }
 }
 
 
